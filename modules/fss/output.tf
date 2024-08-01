2b7dace34527dbdb57622b7591f66f7c84976324
GabrielFeodorov <gabriel.feodorov@oracle.com>
2024-08-01 13:10:44 +0300

    edited fss output

--- modules/fss/output.tf
+++ modules/fss/output.tf
@@ -1,6 +1,11 @@
-output "mt_ocid" {
-  value = oci_file_storage_mount_target.kubeflow_mount_target[0].id
+locals {
+  mt_ocid = var.create_mount_target ? oci_file_storage_mount_target.kubeflow_mount_target[0].id : var.create_mount_target
 }
 
+
+
+output "mt_ocid" {
+  value = local.mt_ocid
+}
 
 
